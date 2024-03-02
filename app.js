b047bebab8fbfa9a2710078bfc8965210dda69b5
Barbara <subwaynaas@yahoo.ie>
2024-03-02 17:31:20 +0200

    changed button

--- app.js
+++ app.js
@@ -7,16 +7,18 @@ console.log(answer);
 const extra = document.querySelector(".extra");
 
 btn.addEventListener("click", (e) => {
-  e.preventDefault();
+  e.preventDefault(); //don't forget this or the innerHTML
+  //message will flash really quick and then disappear
+  //this happens because it is a form so it want to submit by default
   let min = Math.ceil(document.getElementById("begin-number").value);
   let max = Math.floor(document.getElementById("end-number").value);
 
-  random(min, max);
+  random(min, max); //we create this function below
 });
 
 function random(min, max) {
   let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
 
   answer.innerHTML = randomNumber;
-  extra.classList.add("active");
+  extra.classList.add("active"); // to add the bottom line
 }
